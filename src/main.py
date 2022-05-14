4c3162ac1c8f2814da23460eb81d9e5f36d1d477
Image <csmoralesr@gmail.com>
2022-05-13 21:34:24 -0400

    catch err

--- src/main.py
+++ src/main.py
@@ -2,45 +2,289 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+import requests
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Favorites, Item, Character, Planet
+from flask_jwt_extended import (
+    JWTManager,
+    create_access_token,
+    jwt_required,
+    get_jwt_identity,
+)
+
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_CONNECTION_STRING")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+app.config["JWT_SECRET_KEY"] = os.environ.get("FLASK_API_KEY")
+jwt = JWTManager(app)
+
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
+
 # generate sitemap with all your endpoints
-@app.route('/')
+@app.route("/")
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/people', methods=['GET'])
-@app.route('/people/<int:people_id>', methods=["GET"])
-def handle_people(people_id = None):
-    pass
 
-# @app.route("/populate")
-# def handle_populate():
-#     repopulate_characters()
-#     return jsonify({'message': 'ok'}), 200
+@app.route("/user", methods=["GET"])
+@app.route("/user/<int:user_id>", methods=["GET", "PUT", "DELETE"])
+@jwt_required()
+def handle_users(user_id=None):
+    if request.method == "GET":
+        if user_id is not None:
+            user_to_send = User.query.filter_by(id=user_id).one_or_none()
+
+            if user_to_send is not None:
+                return jsonify(user_to_send.serialize()), 200
+            else:
+                return jsonify({"msg": "Not found."}), 404
+        else:
+            all_users = User.query.all()
+            all_users = list(map(lambda usr: usr.serialize(), all_users))
+            return jsonify({"results": all_users}), 200
+
+    if request.method == "PUT":
+        body = request.json
+        email = body.get("email", None)
+
+        if email is not None:
+            user_to_update = User.query.filter_by(id=user_id).first()
+
+            if user_to_update is not None:
+                user_to_update.email = email
+
+                try:
+                    db.session.commit()
+                    return jsonify(user_to_update.serialize()), 201
+                except Exception as error:
+                    db.session.rollback()
+                    return jsonify(error.args)
+        else:
+            return jsonify({"msg": "Not found."}), 404
+
+    if request.method == "DELETE":
+        user_to_delete = User.query.filter_by(id=user_id).first()
+
+        if user_to_delete is not None:
+            db.session.delete(user_to_delete)
+
+            try:
+                db.session.commit()
+                return jsonify([]), 204
+            except Exception as error:
+                db.session.rollback()
+                return jsonify(error.args)
+        else:
+            return jsonify({"msg": "Not found."}), 404
+
+    return "You should not be seeing this message /user"
+
+
+@app.route("/signup", methods=["POST"])
+def handle_signup():
+    body = request.json
+
+    if not body.get("email") or not body.get("password"):
+        return jsonify({"msg": "Not found."}), 404
+    else:
+        user = User(email=body["email"], password=body["password"])
+        db.session.add(user)
+
+        try:
+            db.session.commit()
+            return jsonify(user.serialize()), 201
+        except Exception as error:
+            db.session.rollback()
+            return jsonify(error.args), 500
+
+
+@app.route("/login", methods=["POST"])
+def handle_login():
+    body = request.json
+    email = body.get("email", None)
+    password = body.get("password", None)
+
+    if email is None or password is None:
+        return jsonify({"msg": "Not found."}), 404
+    else:
+        user = User.query.filter_by(email=email, password=password).one_or_none()
+
+        if user is not None:
+            token = create_access_token(identity=user.id)
+            return jsonify({"token": token, "user_id": user.id, "email": user.email})
+        else:
+            return jsonify({"msg": "Not found."}), 404
+
+    return "You should not be seeing this message /login"
+
+
+@app.route("/favorites", methods=["GET", "POST", "DELETE"])
+@jwt_required()
+def handle_favorites():
+    current_user_id = get_jwt_identity()
+
+    if request.method == "GET":
+        all_favorites = Favorites.query.filter_by(user_id=current_user_id)
+        all_favorites = list(map(lambda fav: fav.serialize(), all_favorites))
+
+        pre_item_favorites = []
+
+        for index, favorite in enumerate(all_favorites):
+            item_favorite = Item.query.filter_by(
+                id=all_favorites[index]["item"]
+            ).one_or_none()
+            pre_item_favorites.append(item_favorite)
+
+        all_item_favorites = list(map(lambda fav: fav.serialize(), pre_item_favorites))
+        return jsonify({"results": all_item_favorites}), 200
+
+    if request.method == "POST":
+        body = request.json
+        item = body.get("item_id")
+
+        is_favorited = Favorites.query.filter_by(
+            items=item, user_id=current_user_id
+        ).first()
+
+        if is_favorited is not None:
+            return ({"msg": "Duplicate entry."}), 500
+
+        if not item:
+            return jsonify({"msg": "Not found."}), 404
+        else:
+            favorite = Favorites(user_id=current_user_id, items=item)
+            db.session.add(favorite)
+
+            try:
+                db.session.commit()
+                return jsonify(favorite.serialize()), 201
+            except Exception as error:
+                db.session.rollback()
+                return jsonify(error.args), 500
+
+    if request.method == "DELETE":
+        body = request.json
+        item = body.get("item_id")
+
+        is_favorited = Favorites.query.filter_by(
+            items=item, user_id=current_user_id
+        ).first()
+
+        if is_favorited is not None and item:
+            favorite_to_delete = is_favorited
+            db.session.delete(favorite_to_delete)
+
+            try:
+                db.session.commit()
+                return jsonify([]), 204
+            except Exception as error:
+                db.session.rollback()
+                return jsonify(error.args), 500
+        else:
+            return jsonify({"msg": "Not found."}), 404
+
+
+@app.route("/characters", methods=["GET"])
+@app.route("/characters/<int:char_id>", methods=["GET"])
+def handle_characters(char_id=None):
+    if char_id is not None:
+        character = Character.query.filter_by(character_id=char_id).one_or_none()
+
+        if character is not None:
+            return jsonify(character.serialize())
+        else:
+            return jsonify({"msg": "Not found."}), 404
+    else:
+        all_characters = Character.query.all()
+        all_characters = list(map(lambda char: char.serialize(), all_characters))
+        return jsonify({"results": all_characters}), 200
+
+    return jsonify([]), 200
+
+
+@app.route("/planets", methods=["GET"])
+@app.route("/planets/<int:plan_id>", methods=["GET"])
+def handle_planets(plan_id=None):
+    if plan_id is not None:
+        planet = Planet.query.filter_by(planet_id=plan_id).one_or_none()
+
+        if planet is not None:
+            return jsonify(planet.serialize())
+        else:
+            return jsonify({"msg": "Not found."}), 404
+    else:
+        all_planets = Planet.query.all()
+        all_planets = list(map(lambda plan: plan.serialize(), all_planets))
+        return jsonify({"results": all_planets}), 200
+
+    return jsonify([]), 200
+
+
+BASE_URL = "https://swapi.dev/api"
+
+
+@app.route("/populate/characters", methods=["GET"])
+def handle_populate_characters():
+    response = requests.get(f"{BASE_URL}/{'people'}")
+    body = response.json()
+
+    all_characters = []
+    for result in body["results"]:
+        response = requests.get(result["url"])
+        body = response.json()
+        all_characters.append(body)
+
+    all_instances = []
+    for index, character in enumerate(all_characters):
+        print(character, "character")
+        instance = Character.create(character, index + 1)
+        print(instance, "instance")
+        all_instances.append(instance)
+
+    return jsonify(list(map(lambda ins: ins.serialize(), all_instances))), 200
+
+
+@app.route("/populate/planets", methods=["GET"])
+def handle_populate_planets():
+    response = requests.get(f"{BASE_URL}/{'planets'}")
+    body = response.json()
+
+    all_planets = []
+    for result in body["results"]:
+        response = requests.get(result["url"])
+        body = response.json()
+        all_planets.append(body)
+
+    all_instances = []
+    for index, planet in enumerate(all_planets):
+        print(planet, "planet")
+        instance = Planet.create(planet, index + 1)
+        print(instance, "instance")
+        all_instances.append(instance)
+
+    return jsonify(list(map(lambda ins: ins.serialize(), all_instances))), 200
+
 
 # this only runs if `$ python src/main.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='127.0.0.1', port=PORT, debug=False)
+if __name__ == "__main__":
+    PORT = int(os.environ.get("PORT", 8000))
+    app.run(host="127.0.0.1", port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -11,7 +11,6 @@ class Base(db.Model):
 class User(Base):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     favorites = db.relationship("Favorites", backref="user", uselist=True)
 
@@ -36,6 +35,13 @@ class Favorites(Base):
         db.UniqueConstraint("user_id", "items", name="unique_user_favorites"),
     )
 
+    @classmethod
+    def add_favorite(cls, data):
+        pass
+
+    def serialize(self):
+        return {"item": self.items}
+
 
 class Item(Base):
     id = db.Column(db.Integer, primary_key=True)
@@ -43,29 +49,110 @@ class Item(Base):
     type = db.Column(db.String(20), nullable=False)
     favorites = db.relationship("Favorites", backref="item")
 
+    __table_args__ = (db.UniqueConstraint("name", "type", name="unique_items"),)
+
     __mapper_args__ = {"polymorphic_identity": "item", "polymorphic_on": type}
 
 
 class Character(Item):
     id = db.Column(db.Integer, db.ForeignKey("item.id"), primary_key=True)
-    height = db.Column(db.Integer)
-    gender = db.Column(db.String(20))
-    mass = db.Column(db.Integer)
-    birth_year = db.Column(db.Integer)
-    eye_color = db.Column(db.String(20))
-    skin_color = db.Column(db.String(20))
+    uid = db.Column(db.Integer)
+    height = db.Column(db.String(150))
+    gender = db.Column(db.String(150))
+    mass = db.Column(db.String(150))
+    birth_year = db.Column(db.String(150))
+    skin_color = db.Column(db.String(150))
     # favorites = db.relationship("Favorites", backref="character", uselist=True)
 
     __mapper_args__ = {"polymorphic_identity": "characters"}
 
+    def __init__(self, **kwargs):
+        for (key, value) in kwargs.items():
+            if hasattr(self, key):
+                attr_type = getattr(self.__class__, key).type
+                try:
+                    attr_type.python_type(value)
+                    setattr(self, key, value)
+                except Exception as error:
+                    print(f"Ignore all other attributes: {error}")
+
+    @classmethod
+    def create(cls, data, index):
+        instance = cls(**data, uid=index)
+        if isinstance(instance, cls):
+            db.session.add(instance)
+            try:
+                db.session.commit()
+                print(f"Instance {instance.name} created.")
+                return instance
+            except Exception as error:
+                db.session.rollback()
+                print(error.args)
+        else:
+            print("Something happened. Couldn't create Character instance.")
+            return None
+
+    def serialize(self):
+        return {
+            "item_id": self.id,
+            "name": self.name,
+            "uid": self.uid,
+            "type": self.type,
+            "height": self.height,
+            "gender": self.gender,
+            "mass": self.mass,
+            "birth_year": self.birth_year,
+            "skin_color": self.skin_color,
+        }
+
 
 class Planet(Item):
     id = db.Column(db.Integer, db.ForeignKey("item.id"), primary_key=True)
-    population = db.Column(db.Integer)
-    terrain = db.Column(db.String(80))
-    diameter = db.Column(db.Integer)
-    climate = db.Column(db.String(20))
-    gravity = db.Column(db.String(20))
+    uid = db.Column(db.Integer)
+    population = db.Column(db.String(150))
+    terrain = db.Column(db.String(150))
+    diameter = db.Column(db.String(150))
+    climate = db.Column(db.String(150))
+    gravity = db.Column(db.String(150))
     # favorites = db.relationship("Favorites", backref="planet", uselist=True)
 
     __mapper_args__ = {"polymorphic_identity": "planets"}
+
+    def __init__(self, **kwargs):
+        for (key, value) in kwargs.items():
+            if hasattr(self, key):
+                attr_type = getattr(self.__class__, key).type
+                try:
+                    attr_type.python_type(value)
+                    setattr(self, key, value)
+                except Exception as error:
+                    print(f"Ignore all other attributes: {error}")
+
+    @classmethod
+    def create(cls, data, index):
+        instance = cls(**data, uid=index)
+        if isinstance(instance, cls):
+            db.session.add(instance)
+            try:
+                db.session.commit()
+                print(f"Instance {instance.name} created.")
+                return instance
+            except Exception as error:
+                db.session.rollback()
+                print(error.args)
+        else:
+            print("Something happened. Couldn't create Planet instance.")
+            return None
+
+    def serialize(self):
+        return {
+            "item_id": self.id,
+            "name": self.name,
+            "uid": self.uid,
+            "type": self.type,
+            "population": self.population,
+            "terrain": self.terrain,
+            "diameter": self.diameter,
+            "climate": self.climate,
+            "gravity": self.gravity,
+        }
